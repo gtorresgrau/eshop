a066e4566bf61cbe9972f4674edeaf7cf40305da
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2025-02-08 15:44:40 -0300

    not found

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <div className="flex flex-col items-center">
         <img src="/404.webp" alt="404 Not Found" loading='lazy' title='Pagina no encontrada'/>
-        <h1 className="text-4xl font-bold text-gray-800 mt-6">Página No Encontrada</h1>
+        <h1 className="text-4xl font-bold text-gray-800 mt-6">Página No Encontrada</h1> 
         <p className="text-gray-600 mt-2">Lo sentimos, pero la página que estás buscando no existe.</p>
         <Link href="/" className="mt-4 px-6 py-3 bg-primary text-white rounded-lg hover:bg-primary-hover transition duration-300" title='Volver a inicio'>Volver al Inicio</Link>
       </div>
