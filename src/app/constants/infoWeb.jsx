bb37c8e70b8289cd8cc9ea21807aa38725bf2d21
Gonzalo <gonzalotorresgrau@gmail.com>
2025-02-10 11:21:39 -0300

    servicios ofrecidos

--- src/app/constants/infoWeb.jsx
+++ src/app/constants/infoWeb.jsx
@@ -5,10 +5,13 @@ export const infoWeb = {
     codigoPais:54,
     sobremi:'<strong class="text-text-danger font-bold">ESHOP DEVICES</strong>, es una Startup joven que nos dedicamos a la venta de insumos informaticos, trabajamos por whatsapp para entregas en persona y mercadoshops para quienes quieren envios. ',
     ofrece: [
-        "Envíos a traves de mercadoShops",
-        "Garantía de calidad",
-        "Atención personalizada",
-        "Todos los medios de pago",
+        "Computadoras",
+        "Mini Pc",
+        "Mantenimiento de pc",
+        "Consolas",
+        "Mantenimientos de consolas",
+        "Producotos nuevos y usados",
+        "Accesorios"
       ],
 };
 
