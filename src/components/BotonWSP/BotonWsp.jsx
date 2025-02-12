92c8ddec2e9b895bf53f8a90298a101a1ca2ac96
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2025-02-11 23:44:57 -0300

    setIsHovered

--- src/components/BotonWSP/BotonWsp.jsx
+++ src/components/BotonWSP/BotonWsp.jsx
@@ -3,43 +3,28 @@
 import { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import userData from "@/app/constants/userData";
-import Link from "next/link";
 
-// crear userData en /constants
-// const userData = {
-//   name:'Starcam',
-//   contact:2235032141,
-//   codigoPais:54,
-//   textBoton:'¡Contáctame!',
-//   email:'starcaminfo@gmail.com',
-//   textoPredefinido:'Hola, me gustaria saber mas sobre, '
-// };
-
-// export default userData;
-
-const BotonWsp = ({texto}) => {
+const BotonWsp = ({ texto }) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
-
-  const enviar = `https://wa.me/+${userData.codigoPais}${userData.contact}?text=${encodeURIComponent(texto?texto:userData.textoPredefinido)}`;
+  const enviar = `https://wa.me/+${userData.codigoPais}${userData.contact}?text=${encodeURIComponent(texto || userData.textoPredefinido)}`;
 
   return (
-      <article className="fixed bottom-6 right-6 z-40">
-        <Link href={enviar} passHref title="Boton de contacto WhatsApp">
-          <a className="flex items-center justify-center bg-green-500 text-white font-bold p-4 rounded-full" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}  aria-label="Contact via WhatsApp" >
-            <FaWhatsapp className='text-white text-3xl' aria-label="Boton de contacto WhatsApp" />
-            {isHovered && <h2 className='ml-2'>{userData.textBoton}</h2>}
-          </a>
-        </Link>
-      </article>
-
+    <article className="fixed bottom-6 right-6 z-40">
+      <a 
+        href={enviar} 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        title="Botón de contacto WhatsApp"
+        className="flex items-center justify-center bg-green-500 text-white font-bold p-4 rounded-full"
+        onMouseEnter={() => setIsHovered(true)} 
+        onMouseLeave={() => setIsHovered(false)} 
+        aria-label="Contact via WhatsApp"
+      >
+        <FaWhatsapp className="text-white text-3xl" aria-label="Botón de contacto WhatsApp" />
+        {isHovered && <h2 className="ml-2">{userData.textBoton}</h2>}
+      </a>
+    </article>
   );
 }
 
