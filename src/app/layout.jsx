688f1343a57333a39c23ab25362311a8f00f63e1
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2025-02-16 03:45:21 -0300

    12TKRXLHEQ

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -101,13 +101,13 @@ export default function RootLayout({ children }) {
           }}
         />
         {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7LRLRDC81W"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-12TKRXLHEQ"></script>
         <script dangerouslySetInnerHTML={{
             __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-7LRLRDC81W'{
+                gtag('config', 'G-12TKRXLHEQ'{
                   page_path: window.location.pathname,
                 });
             `,
