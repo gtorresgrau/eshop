eb70affc37c4db6aa9ace861ce452e21cf9a1d38
Gonzalo <gonzalotorresgrau@gmail.com>
2025-02-17 15:01:01 -0300

    card

--- src/components/Tienda/Card/Card.jsx
+++ src/components/Tienda/Card/Card.jsx
@@ -31,7 +31,7 @@ const Card = ({ product, handleProductSelect }) => {
   )}`;
 
   return (
-    <li className="relative w-44 sm:w-48 md:w-64 lg:w-56 xl:w-72 lg:h-80 xl:h-96 min-h-[320px] md:min-h-[430px] lg:min-h-[420px] xl:min-h-[465px] cursor-pointer">
+    <li className="relative xs:w-44 sm:w-48 md:w-64 lg:w-56 xl:w-72 lg:h-80 xl:h-96 md:min-h-[320px] lg:min-h-[360px] xl:min-h-[420px] list-none cursor-pointer">
       <div
         className="relative flex flex-col justify-between w-full h-full bg-white border border-gray-200 rounded-lg shadow hover:shadow-lg transition-shadow duration-300"
         onClick={() => handleProductSelect(product)}
@@ -40,7 +40,7 @@ const Card = ({ product, handleProductSelect }) => {
         onKeyDown={(e) => e.key === 'Enter' && handleProductSelect(product)}
       >
         <div>
-          <div className="relative aspect-[4/3]">
+          <div className="relative aspect-square">
             {product.vendido && (
               <p className="absolute inset-0 flex items-center justify-center text-white text-3xl font-bold bg-red-400 bg-opacity-80 rounded-t-lg grayscale">
                 VENDIDO
@@ -54,10 +54,10 @@ const Card = ({ product, handleProductSelect }) => {
               <IconShoopingCart ancho={20} alto={20} color="#ffffff" />
             </button>
             <Image
-              className="rounded-t-lg w-full h-full object-cover aspect-[4/3]"
+              className="rounded-t-lg object-cover "
               src={product.foto_1_1 || '/images/sinFoto.webp'}
               alt={product.nombre}
-              width={400}
+              width={300}
               height={300}
               loading="lazy"
             />
@@ -78,34 +78,98 @@ const Card = ({ product, handleProductSelect }) => {
             </div>
           </div>
           <div className="px-4 py-1">
-            <h2 className="text-sm font-semibold text-gray-900 md:text-base md:font-bold">{product.nombre}</h2>
+            <h2 className="text-sm font-semibold text-gray-900 md:text-base md:font-bold text-center">{product.nombre}</h2>
             <p className="text-xs text-gray-700 md:text-base">
               <strong>Marca:</strong> {product.marca}
             </p>
             <p className="text-xs text-gray-700 md:text-base">
-              <strong>Categoría:</strong> {product.categoria}
+              <strong>Categoría:</strong>  {product.categoria.length > 10 ? `${product.categoria.slice(0, 10)}...` : product.categoria}
             </p>
           </div>
         </div>
-        <div className="px-2 pb-1 flex justify-between gap-2">
-          <Link
-            href={product.n_electronica || '#'}
-            passHref
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition"
-          >
-            <MdStore size={16} /> <span>Ver en Shop</span>
-          </Link>
-          <button
-            onClick={handleConsult}
-            className={`flex items-center gap-2 px-4 py-2 text-white rounded-lg shadow transition ${
-              product.vendido ? 'bg-slate-500' : 'bg-primary-whats hover:bg-primary-whatsHover'
-            }`}
-            disabled={!!product.vendido}
-          >
-            <RiWhatsappLine size={16} /> <span>Consultar</span>
-          </button>
+        <div className="px-2 pb-1 flex  gap-2 justify-center">
+          {product.n_electronica ? (
+            <>
+              {/* Para pantallas md (menos de xl) */}
+              <div className="hidden md:flex xl:hidden justify-around gap-2 text-xs xl:text-sm self-center">
+                <Link href={product.n_electronica} passHref
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Ver producto en Shop"
+                    aria-label="Ver producto en Shop"
+                    className="flex items-center w-1/2 gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition duration-200"
+                  >
+                    <MdStore size={16} /> <span>Shop..</span>
+                </Link>
+                <button
+                  onClick={handleConsult}
+                  className={`flex items-center gap-2 px-4 py-2 w-1/2  text-white rounded-lg shadow transition duration-300 ${product.vendido?'bg-slate-500':'bg-primary-whats hover:bg-primary-whatsHover'}`}
+                  title="Consultar por WhatsApp"
+                  aria-label="Consultar por WhatsApp"
+                  type="button"
+                >
+                  <RiWhatsappLine size={16} /> <span>Cons...</span>
+                </button>
+              </div>
+              {/* Para pantallas xl */}
+              <div className="hidden xl:flex justify-around gap-2 text-xs xl:text-sm">
+                <Link href={product.n_electronica} passHref
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Ver producto en MercadoShop"
+                    aria-label="Ver producto en MercadoShop"
+                    className="flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition duration-200"
+                  >
+                    <MdStore size={16} /> <span>MercadoShop</span>
+                </Link>
+                <button
+                  onClick={handleConsult}
+                  className={`flex items-center gap-2 px-4 py-2 text-white rounded-lg shadow transition duration-300 ${product.vendido?'bg-slate-500':'bg-primary-whats hover:bg-primary-whatsHover'}`}
+                  title="Consultar por WhatsApp"
+                  aria-label="Consultar por WhatsApp"
+                  type="button"
+                  disabled={product.vendido?true:false}
+                >
+                  <RiWhatsappLine size={16} /> <span>Consultar</span>
+                </button>
+              </div>
+              {/* Para pantallas pequeñas */}
+              <div className="flex md:hidden justify-around gap-2">
+                <Link href={product.n_electronica} passHref
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Ver producto en Shop"
+                    aria-label="Ver producto en Shop"
+                    className="flex items-center justify-center w-16 h-8 bg-blue-600 text-white rounded-md shadow hover:bg-blue-700 transition duration-200"
+                  >
+                    <MdStore size={16} />
+                </Link>
+                <button
+                  onClick={handleConsult}
+                  className={`flex items-center justify-center w-16 h-8  text-white rounded-md shadow transition duration-300 ${product.vendido?'bg-slate-500':'bg-primary-whats hover:bg-primary-whatsHover'}`}
+                  title="Consultar por WhatsApp"
+                  aria-label="Consultar por WhatsApp"
+                  type="button"
+                  disabled={product.vendido?true:false}
+
+                >
+                  <RiWhatsappLine size={16} />
+                </button>
+              </div>
+            </>
+          ) : (
+            <button
+              onClick={handleConsult}
+              className={`flex items-center justify-center w-full px-4 py-2 gap-2   text-white rounded-lg shadow  transition duration-300 ${product.vendido?'bg-slate-500':'bg-primary-whats hover:bg-primary-whatsHover'}`}
+              title="Consultar por WhatsApp"
+              aria-label="Consultar por WhatsApp"
+              type="button"
+              disabled={product.vendido?true:false}
+
+            >
+              <RiWhatsappLine size={16} /> <span>Consultar</span>
+            </button>
+              )}
         </div>
       </div>
     </li>
