c7113096311667207a67dfda628eb443f8714f03
Gonzalo Torres Grau <gonzalotorresgrau@gmail.com>
2025-02-18 00:49:33 -0300

    descripcion

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,8 +5,8 @@ import { ShoppingCartProvider } from '@/components/Context/ShoopingCartContext';
 
 export const metadata = {
   title: 'Eshop Devices Wilde Insumos Informáticos profesionales y standard',
-  description: "Eshop Devices es tu aliado tecnológico en Wilde, especializado en la venta de insumos informáticos y dispositivos electrónicos. Con atención personalizada y asesoramiento experto, te ayudamos a encontrar la solución ideal para el hogar y negocio. Descubre componentes, accesorios y gadgets de última generación.",
-  keywords: 'repuestos para computadoras, ecommerce, eshop devices, dispositivos informaticos, insumos de informatica, memorias, ssd, disco solido, mvne',
+  description: "Eshop Devices es tu aliado tecnológico en Wilde, especializado en la venta de insumos informáticos y dispositivos electrónicos. Con atención personalizada y asesoramiento experto, la solución ideal para el hogar y negocio.Componentes, accesorios y gadgets de última generación como la fuente adaptador para Starlink Mini",
+  keywords: 'repuestos para computadoras, ecommerce, eshop devices, dispositivos informaticos, insumos de informatica, memorias, ssd, disco solido, mvne, Starlink Mini, cable adaptador, elevador de tension para Starlink Mini - Trifix SL1230, mejora conexión satelital, conexion en ruta, elevador de tension 12V a 35V, fuente para starlink mini, fuente adaptador para conexion satelital',
   charSet: 'UTF-8',
   icons: '/favicon.ico',
   manifest: '/manifest.json',
@@ -15,7 +15,7 @@ export const metadata = {
   publisher: 'Eshop Devices Wilde Insumos Informáticos profesionales y standard',
   openGraph: {
     title: 'Eshop Devices Wilde Insumos Informáticos profesionales y standard',
-    description: "Eshop Devices es tu aliado tecnológico en Wilde, especializado en la venta de insumos informáticos y dispositivos electrónicos. Con atención personalizada y asesoramiento experto, te ayudamos a encontrar la solución ideal para el hogar y negocio. Descubre componentes, accesorios y gadgets de última generación.",
+    description: "Eshop Devices es tu aliado tecnológico en Wilde, especializado en la venta de insumos informáticos y dispositivos electrónicos. Con atención personalizada y asesoramiento experto, la solución ideal para el hogar y negocio.Componentes, accesorios y gadgets de última generación como la fuente adaptador para Starlink Mini",
     type: 'website',
     url: 'https://eshopdevices.com/',
     image: 'https://eshopdevices.com/logos/logoEshop.webp',
