4f47a18f8bef57e8035ae986f130f4e4c01f6552
gtorresgrau <gonzalotorresgrau@gmail.com>
2025-02-23 06:24:43 -0300

    header original

--- src/components/Banner/Header.jsx
+++ src/components/Banner/Header.jsx
@@ -1,27 +1,16 @@
-'use client'
-import React, { Suspense } from 'react';
-import dynamic from 'next/dynamic';
-import Loading from '../Loading/Loading';
-
-// Cargar StarlinkMiniBanner de forma dinámica para evitar bloquear el render del H1
-const StarlinkMiniBanner = dynamic(() => import('../Productos/Starlink/bannerStarLink'), {
-  ssr: false, // Solo en cliente para evitar retrasos en el servidor
-  loading: () => <Loading />, // Placeholder
-});
+// Header.js
+import React from 'react';
+import StarlinkMiniBanner from '../Productos/Starlink/bannerStarLink';
 
 export default function Header() {
   return (
     <>
       <div className="text-center py-4 max-w-6xl mx-auto">
-        <h1 className="text-lg text-gray-600 px-2 md:text-center">
+        <h1 className="text-lg text-gray-600 px-2 text-justify md:text-center">
           Encuentra los mejores productos y servicios informáticos aquí: <strong>PC Gamers, PC mini para oficinas, PC profesionales</strong> y muchos accesorios para informática como la <strong>fuente adaptador de STARLINK mini.</strong> Además contamos con Servicio de Mantenimiento de Consolas, y armado de computadoras según tu necesidad.
         </h1>
       </div>
-      
-      {/* Suspense envuelve el banner para que el H1 se renderice primero */}
-      <Suspense fallback={<Loading/> }>
-        <StarlinkMiniBanner />
-      </Suspense>
+      <StarlinkMiniBanner />
     </>
   );
 }
