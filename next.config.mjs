680b22e52e0a629c2071d87658cb232fc6794505
Gonzalo <gonzalotorresgrau@gmail.com>
2025-02-26 15:22:11 -0300

    import withBundleAnalyzer from '@next/bundle-analyzer';

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,14 @@
-//next.config.mjs
-/** @type {import('next').NextConfig} */
+// next.config.mjs
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-  });
-  
 const nextConfig = {
-    images: {
-        loader: 'cloudinary',
-        path: 'https://res.cloudinary.com/dnbrxpca3/',
-        domains: ['localhost', 'res.cloudinary.com', 'eshopdevices.vercel.app', 'eshopdevices.com'],  // Agrega dominios adicionales si es necesario
-        unoptimized: true
-    },
-    compress: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dnbrxpca3/',
+    domains: ['localhost', 'res.cloudinary.com', 'eshopdevices.vercel.app', 'eshopdevices.com'],
+    unoptimized: true,
+  },
+  compress: true,
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default withBundleAnalyzer({ enabled: process.env.ANALYZE === 'true' })(nextConfig);
