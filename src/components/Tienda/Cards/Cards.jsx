b804417efafab7a8ecda3d8172035bce3fda906b
Gonzalo <gonzalotorresgrau@gmail.com>
2025-02-26 13:35:01 -0300

    cards

--- src/components/Tienda/Cards/Cards.jsx
+++ src/components/Tienda/Cards/Cards.jsx
@@ -3,82 +3,82 @@ import Card from '../Card/Card';
 import SkeletonCard from '../Card/SkeletonCard';
 
 const Cards = ({ 
-  //products, 
+  products, 
   handleProductSelect,
   isLoading
  }) => {
-  const products = [
-    {
-      cod_producto: "P001",
-      n_producto: 1,
-      marca: "Zeiss",
-      categoria: "Lentes",
-      nombre: "Lente Intraocular",
-      modelo: "AcrySof IQ",
-      n_serie: "12345ABC",
-      titulo_de_producto: "Lente Intraocular Premium",
-      descripcion: "Lente intraocular monofocal con filtro de luz azul.",
-      n_electronica: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
-      precio: "1500",
-      destacados: true,
-      usd: true,
-      usado: false,
-      vendiod:false,
-      medidas: "12mm x 6mm",
-      foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
-      foto_1_2: "url2.jpg",
-      foto_1_3: "url3.jpg",
-      foto_1_4: "url4.jpg"
-    },
-    {
-      cod_producto: "P002",
-      n_producto: 2,
-      marca: "Topcon",
-      categoria: "Equipamiento",
-      nombre: "Autorefractómetro",
-      modelo: "KR-800",
-      n_serie: "67890XYZ",
-      titulo_de_producto: "Autorefractómetro de Alta Precisión",
-      descripcion: "Dispositivo para medir la refracción ocular con precisión avanzada.",
-      n_electronica: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
-      precio: "8500",
-      destacados: false,
-      usd: true,
-      usado: true,
-      vendiod:true,
-      medidas: "40cm x 30cm x 50cm",
-      foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
-      foto_1_2: "url6.jpg",
-      foto_1_3: "url7.jpg",
-      foto_1_4: "url8.jpg"
-    },
-    {
-      cod_producto: "P003",
-      n_producto: 3,
-      marca: "Heine",
-      categoria: "Instrumentos",
-      nombre: "Oftalmoscopio",
-      modelo: "Beta 200",
-      n_serie: "11223LMN",
-      titulo_de_producto: "Oftalmoscopio de Diagnóstico",
-      descripcion: "Instrumento de diagnóstico con iluminación LED y ajuste de enfoque.",
-      n_electronica: "",
-      precio: "1200",
-      destacados: true,
-      usd: false,
-      usado: false,
-      vendiod:false,
-      medidas: "18cm x 5cm",
-      foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
-      foto_1_2: "url10.jpg",
-      foto_1_3: "url11.jpg",
-      foto_1_4: "url12.jpg"
-    }
-  ];
+  // const products = [
+  //   {
+  //     cod_producto: "P001",
+  //     n_producto: 1,
+  //     marca: "Zeiss",
+  //     categoria: "Lentes",
+  //     nombre: "Lente Intraocular",
+  //     modelo: "AcrySof IQ",
+  //     n_serie: "12345ABC",
+  //     titulo_de_producto: "Lente Intraocular Premium",
+  //     descripcion: "Lente intraocular monofocal con filtro de luz azul.",
+  //     n_electronica: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
+  //     precio: "1500",
+  //     destacados: true,
+  //     usd: true,
+  //     usado: false,
+  //     vendiod:false,
+  //     medidas: "12mm x 6mm",
+  //     foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
+  //     foto_1_2: "url2.jpg",
+  //     foto_1_3: "url3.jpg",
+  //     foto_1_4: "url4.jpg"
+  //   },
+  //   {
+  //     cod_producto: "P002",
+  //     n_producto: 2,
+  //     marca: "Topcon",
+  //     categoria: "Equipamiento",
+  //     nombre: "Autorefractómetro",
+  //     modelo: "KR-800",
+  //     n_serie: "67890XYZ",
+  //     titulo_de_producto: "Autorefractómetro de Alta Precisión",
+  //     descripcion: "Dispositivo para medir la refracción ocular con precisión avanzada.",
+  //     n_electronica: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
+  //     precio: "8500",
+  //     destacados: false,
+  //     usd: true,
+  //     usado: true,
+  //     vendiod:true,
+  //     medidas: "40cm x 30cm x 50cm",
+  //     foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
+  //     foto_1_2: "url6.jpg",
+  //     foto_1_3: "url7.jpg",
+  //     foto_1_4: "url8.jpg"
+  //   },
+  //   {
+  //     cod_producto: "P003",
+  //     n_producto: 3,
+  //     marca: "Heine",
+  //     categoria: "Instrumentos",
+  //     nombre: "Oftalmoscopio",
+  //     modelo: "Beta 200",
+  //     n_serie: "11223LMN",
+  //     titulo_de_producto: "Oftalmoscopio de Diagnóstico",
+  //     descripcion: "Instrumento de diagnóstico con iluminación LED y ajuste de enfoque.",
+  //     n_electronica: "",
+  //     precio: "1200",
+  //     destacados: true,
+  //     usd: false,
+  //     usado: false,
+  //     vendiod:false,
+  //     medidas: "18cm x 5cm",
+  //     foto_1_1: "https://res.cloudinary.com/dnbrxpca3/image/upload/v1737477252/Products/m1v4k7jnfsxzglqhr6gy.webp",
+  //     foto_1_2: "url10.jpg",
+  //     foto_1_3: "url11.jpg",
+  //     foto_1_4: "url12.jpg"
+  //   }
+  // ];
 
   return (
     <ul className="grid grid-cols-2 lg:grid-cols-3 gap-4 py-1 w-full mx-auto md:max-w-7xl px-2 sm:px-6 lg:px-8 justify-items-center">
-      {false ? (
+      {isLoading ? (
             <>
                 {[...Array(9)].map((_, i) => (
                     <li key={i} className="w-full">
