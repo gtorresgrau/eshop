91c1166de510f4d400b10fdcd403b8b75c83cccf
gtorresgrau <gonzalotorresgrau@gmail.com>
2025-02-28 18:43:50 -0300

    Footer

--- src/app/home/FooterSection.jsx
+++ src/app/home/FooterSection.jsx
@@ -1,11 +1,14 @@
-'use client';
-import React from 'react';
-import Footer from '../../components/Footer/Footer';
-import VolverArriba from '../../components/VolverArriba/VolverArriba';
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+
+const Footer = dynamic(() => import("@/components/Footer/Footer"));
+const VolverArriba = dynamic(() => import("@/components/VolverArriba/VolverArriba"), { ssr: false });
 
 export default function FooterSection() {
   return (
-    <footer>
+    <footer className="flex flex-col">
       <Footer />
       <VolverArriba />
     </footer>
