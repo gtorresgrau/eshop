a9b879c7ba2f06d131fdb562cb88e600ff8c933b
gtorresgrau <gonzalotorresgrau@gmail.com>
2025-03-05 23:55:12 -0300

    no metadata

--- src/app/productos/[nombre]/page.jsx
+++ src/app/productos/[nombre]/page.jsx
@@ -4,8 +4,8 @@ import React, { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { useParams } from 'next/navigation';
 import useProductModal from '@/Hooks/useProductModal';
-import { useMetadata, defaultMetadata } from '@/Hooks/useMetadata';
-import Head from 'next/head';
+//import { useMetadata, defaultMetadata } from '@/Hooks/useMetadata';
+//import Head from 'next/head';
 
 const Modal = dynamic(() => import('@/components/Tienda/Modal/Modals'));
 const Loading = dynamic(() => import('@/components/Loading/Loading'));
@@ -39,17 +39,17 @@ const ProductoPage = () => {
     return <Loading />;
   }
 
-// Mueve la llamada a useMetadata fuera de la condición
-const metadata = useMetadata({
-  title: selectedProduct?.name || defaultMetadata.title,
-  description: selectedProduct ? `${selectedProduct.marca} ${selectedProduct.categoria}` : defaultMetadata.description,
-  keywords: selectedProduct?.titulo_de_producto || defaultMetadata.keywords,
-  image: selectedProduct?.foto_1_1 || defaultMetadata.openGraph.image,
-});
+// // Mueve la llamada a useMetadata fuera de la condición
+// const metadata = useMetadata({
+//   title: selectedProduct?.name || defaultMetadata.title,
+//   description: selectedProduct ? `${selectedProduct.marca} ${selectedProduct.categoria}` : defaultMetadata.description,
+//   keywords: selectedProduct?.titulo_de_producto || defaultMetadata.keywords,
+//   image: selectedProduct?.foto_1_1 || defaultMetadata.openGraph.image,
+// });
 
   return (
     <ClientLayout className="flex flex-col h-screen" title={selectedProduct.name}>
-      <Head>
+      {/* <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
         <meta name="keywords" content={metadata.keywords} />
@@ -58,8 +58,8 @@ const metadata = useMetadata({
         <meta property="og:image" content={metadata.openGraph.images[0].url} />
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href={metadata.canonical} />
-        {/* Otros metadatos que quieras agregar */}
-      </Head>
+        {/* Otros metadatos que quieras agregar 
+       </Head> */} 
       <main className="flex-1 flex items-center justify-center bg-white">
         <Modal selectedProduct={selectedProduct} closeModal={closeModal} isDialog={false} />
       </main>
