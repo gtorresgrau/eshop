93ba7730560529569be2e146793a1a0aefbeb21d
Gonzalo <gonzalotorresgrau@gmail.com>
2025-08-06 14:03:42 -0300

    paquetes

---
+++ src/components/constants/userBank.jsx
@@ -0,0 +1,9 @@
+const userBank ={
+    banco: 'Mercado Pago',
+    cbu: '0000003100003176461967',
+    alias: 'eshopdevices.mp',
+    titular: 'Gonzalo Torres Grau',
+    cuit: '23-33108373-9',
+}
+
+export default userBank;
