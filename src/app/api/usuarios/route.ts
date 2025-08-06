5a88d725b2b0b52984f8dafaa83b9d3fdb3f1634
Gonzalo <gonzalotorresgrau@gmail.com>
2025-08-06 14:38:50 -0300

    cambios

--- src/app/api/usuarios/route.ts
+++ src/app/api/usuarios/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import { connectDB } from '../../../lib/mongodb';
 import Usuario from '../../../models/User';
 import jwt from 'jsonwebtoken';
-import { auth } from '../../../lib/firebaseAdminConfig';
+import { auth } from '../../../../pages/api/firebaseAdminConfig';
 
 export async function POST(req: Request) {
     try {
